74495c90104a2eebfa9f0e695bd07cfc4391f2fb
Mykerro <mykecamillo05@gmail.com>
2023-05-18 12:28:27 -0300

    falar

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,4 +2,8 @@ public class Pessoa {
     // Atributos da pessoa
     public String nome;
     public String sobrenome;
+    // Métodos da classe
+    public void falar(){
+        System.out.println("falei");
+    }
 }
