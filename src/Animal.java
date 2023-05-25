8f27a4fc2f134913eed2f5cd21adfe2dab4f0a11
Mykerro <mykecamillo05@gmail.com>
2023-05-25 12:30:47 -0300

    comer

---
+++ src/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    // Atributos do objeto
+    String especie;
+    String nome;
+    String raca;
+    String peso;
+    String corPele;
+    String corOlhoDireito;
+    // Métodos
+    public void comer(){
+        System.out.println("Comeu como animal");
+    }
+
+}
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,9 +1,15 @@
-public class Pessoa {
-    // Atributos da pessoa
-    public String nome;
+public class Pessoa extends Animal {
+    // Atributos da classe
     public String sobrenome;
     // Métodos da classe
     public void falar(){
         System.out.println("falei");
+    }
+    // Sobrecarga do método falar()
+    public String falar(String volume){
+        return "falei " + volume;
+    }
+    public void comer(){
+        System.out.println("Comeu como pessoa");
     }
 }
